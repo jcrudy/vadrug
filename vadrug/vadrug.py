03605442382106ecc928211ba14909495bb41b22
Jason Rudy <jcrudy@gmail.com>
2017-05-18 14:10:58 -0700

    Fix some bugs

--- vadrug/vadrug.py
+++ vadrug/vadrug.py
@@ -5,11 +5,16 @@ from clinvoc.ndc import NDC
 import pickle
 from clinvoc.code_collections import CodeCollection
 from clinvoc.base import left_pad
+from . import __version__
+from ._version import get_versions
 
 def _process_drug_file():
     try:
         with open(os.path.join(resources, 'cache.pickle'), 'rb') as infile:
-            return pickle.load(infile)
+            result, v = pickle.load(infile)
+        assert v == __version__
+        assert not get_versions()['dirty']
+        return result
     except:
         ndf = pandas.read_excel(os.path.join(resources, 'NDF_January_2016.xlsx'), sheetname='Sheet1', )
         vocab = NDC()
@@ -26,7 +31,7 @@ def _process_drug_file():
             cls = row_['VA_CLASS']
             ndc = vocab.standardize(left_pad('%.11s' % int(row_['NDF_NDC']), 11))
             
-            if cls in va_class:
+            if (cls, vocab.vocab_name) in va_class:
                 va_class[(cls, vocab.vocab_name)].add(ndc)
             else:
                 va_class[(cls, vocab.vocab_name)] = {ndc}
@@ -37,7 +42,7 @@ def _process_drug_file():
     #     del va_class['NONSALICYLATE NSAIS,ANTIRHEUMATIC']
     #         
         with open(os.path.join(resources, 'cache.pickle'), 'wb') as outfile:
-            pickle.dump(va_class, outfile)
+            pickle.dump((va_class, __version__), outfile)
         return va_class
 
 # Not attempting to map between "classes" and "categories" because, upon inspection, there are repeats in both columns
